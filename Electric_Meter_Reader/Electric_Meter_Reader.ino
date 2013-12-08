55393a8c3cbfa20bb74eacbabf759851c269bf52
sushyad <sushyad@gmail.com>
2013-12-07 23:53:42 -0500

    Modified to use aanon4's RMF69_COMPAT code for RFM12B

--- Electric_Meter_Reader/Electric_Meter_Reader.ino
+++ Electric_Meter_Reader/Electric_Meter_Reader.ino
@@ -1,56 +1,23 @@
-/*
-  WaterMote.ino - Arduino/Moteino sketch for reading a SY310 based water/pulse meter
-  Copyright (c) 2013 Felix Rusu (felix@lowpowerlab.com).  All rights reserved.
-
-  This code is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This code is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
+#include "config.h"
+#include "debug.h"
+#include <LowPower.h>
+#if USE_RFM12B
+#include <RFM12B.h>
+#elif USE_RFM69
 #include <RFM69.h>
 #include <SPI.h>
-#include <LowPower.h>
+#endif
+#include <Node.h>
 #include <EEPROM.h>
 #include <TimerOne.h>
 
-#define NODEID        18    //unique for each node on same network
-#define NETWORKID     100  //the same on all nodes that talk to each other
-#define GATEWAYID     1
-//Match frequency to the hardware version of the radio on your Moteino (uncomment one):
-//#define FREQUENCY     RF69_433MHZ
-//#define FREQUENCY     RF69_868MHZ
-#define FREQUENCY     RF69_433MHZ
-#define ENCRYPTKEY    "xxxxxxxxxxxxxxxx" //exactly the same 16 characters/bytes on all nodes!
-#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
-#define ACK_TIME      30 // max # of ms to wait for an ack
+NodeClass Node;
+
 #define LED           9
+
+static void command(byte cmd, long arg, byte len, byte* raw);
+
 #define INPUTPIN      1  //INT0 = D2, INT1 = digital pin 3 (must be an interrupt pin!)
-
-int TRANSMITPERIOD = 300; //transmit a packet to gateway so often (in ms)
-RFM69 radio;
-
-#define SERIAL_EN        //uncomment this line to enable serial IO (when you debug Moteino and need serial output)
-#define SERIAL_BAUD  115200
-#ifdef SERIAL_EN
-  #define DEBUG(input)   {Serial.print(input);}
-  #define DEBUGln(input) {Serial.println(input);}
-#else
-  #define DEBUG(input);
-  #define DEBUGln(input);
-#endif
-
-typedef struct { int nodeId; float kWh; float wlm; long pulseCount;} PayloadTX;
-PayloadTX emontx; // neat way of packaging data for RF comms
 
 int ledState = LOW;
 volatile unsigned long PulseCounterVolatile = 0; // use volatile for shared variables
@@ -77,33 +44,34 @@ float AVGKW=0;
 char sendBuf[50];
 byte sendLen;
 
-void setup() {
-  emontx.nodeId = NODEID;
+#if defined(KEY_ELECTRIC_METER_READING)
+  static byte last_KEY_ELECTRIC_METER_READING;  
+#endif
+
+static byte get_update(bool force_update);
+
+void setup(void)
+{
+#if SERIAL_BAUD
+  Serial.begin(SERIAL_BAUD);
+#endif
+
   pinMode(LED, OUTPUT);
   
-  radio.initialize(FREQUENCY,NODEID,NETWORKID);
-#ifdef IS_RFM69HW
-  radio.setHighPower(); //uncomment only for RFM69HW!
-#endif
-  //radio.encrypt(ENCRYPTKEY);
-
+  // Initialize the node
+  Node.initialize(NODEID, MASTERID, NETWORKID, command);
+  Node.setPowerManagement(false);
+  
   //initialize counter from EEPROM
-  //unsigned long savedCounter = EEPROM_Read_Counter();
-  unsigned long savedCounter = 0;
+  unsigned long savedCounter = EEPROM_Read_Counter();
+  //unsigned long savedCounter = 0;
   if (savedCounter <=0) savedCounter = 1; //avoid division by 0
   PulseCounterVolatile = PulseCounter = PULSECOUNTLASTMINUTEMARK = savedCounter;
   attachInterrupt(INPUTPIN, pulseCounter, FALLING);
   Timer1.initialize(XMIT_Interval * 1000L);
   Timer1.attachInterrupt(XMIT);
-  
-  char buff[50];
-  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
-  Serial.println(buff);
-  
-  #ifdef SERIAL_EN
-    Serial.begin(SERIAL_BAUD);
-    DEBUGln("\nReading...");
-  #endif
+
+  DEBUGLN("Will start counting pulses.");
 }
 
 void XMIT(void)
@@ -113,16 +81,15 @@ void XMIT(void)
   interrupts();
   
   if (millis() - TIMESTAMP_pulse_curr >= 5000) {
-    //ledState = !ledState; digitalWrite(LED, ledState);
-    blink(2);
+    ledState = !ledState; digitalWrite(LED, ledState);
   }
 
   //calculate Gallons counter 
   KWH = ((float)PulseCounter) * Kh / 1000;
-  emontx.pulseCount = PulseCounter; 
-  emontx.kWh = KWH;
+  //emontx.pulseCount = PulseCounter; 
+  //emontx.kWh = KWH;
   
-  DEBUG("PulseCounter:");DEBUG(PulseCounter);DEBUG(", KWH: "); DEBUGln(KWH);
+  DEBUG("PulseCounter:");DEBUG(PulseCounter);DEBUG(", KWH: "); DEBUGLN(KWH);
   
   String tempstr = String("KWH:");
   tempstr += (unsigned long)KWH;
@@ -130,8 +97,7 @@ void XMIT(void)
   tempstr += ((unsigned long)(KWH * 100)) % 100;
 
   //calculate & output KWH
-  AVGKW = pulseAVGInterval > 0 ? 60.0 * 1000 * (1.0/PulsesPerKWH)/(pulseAVGInterval/pulsesPerXMITperiod)
-                             : 0;
+  AVGKW = pulseAVGInterval > 0 ? 60.0 * 1000 * (1.0/PulsesPerKWH)/(pulseAVGInterval/pulsesPerXMITperiod) : 0;
   pulsesPerXMITperiod = 0;
   pulseAVGInterval = 0;
     
@@ -141,24 +107,98 @@ void XMIT(void)
   {
     secondCounter=0;
     float WLM = ((float)(PulseCounter - PULSECOUNTLASTMINUTEMARK)) * Kh ;
-    emontx.wlm = WLM;
+    //emontx.wlm = WLM;
     PULSECOUNTLASTMINUTEMARK = PulseCounter;
     EEPROM_Write_Counter(PulseCounter);
   }
 
-  if (radio.sendWithRetry(GATEWAYID, (const void*)(&emontx), sizeof(emontx))) {
-    DEBUG("Sent packet");
-  }
-  else {
-      DEBUG("Unable to send packet");
-  }
+  //get_update(Node.force_update(true));
+  Node.send(get_update(true));
   
-  radio.sleep();
-  
-  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  Node.sleep(SLEEP_8S);
 }
 
 void loop() {}
+
+static byte get_update(bool force_update)
+{
+  byte* payload = Node.get_payload();
+  int c = 0;
+#define  KV(K,V) do { \
+  if (force_update || last_##K != (V)) \
+  { \
+    payload[c++] = 2; \
+    payload[c++] = (K); \
+    payload[c++] = (V); \
+    last_##K = (V); \
+  } \
+} while (0)
+#define  KV2(K,V) do { \
+  if (force_update || last_##K != (V)) \
+  { \
+    payload[c++] = 3; \
+    payload[c++] = (K); \
+    payload[c++] = ((V) >> 8); \
+    payload[c++] = (V); \
+    last_##K = (V); \
+  } \
+} while (0)
+
+#if defined(KEY_ELECTRIC_METER_READING)
+  DEBUGLN("About to send meter reading...");
+  KV(KEY_ELECTRIC_METER_READING, PulseCounter);
+#endif
+
+  return c;
+}
+
+static void command(byte cmd, long arg, byte len, byte* raw)
+{
+  DEBUG("cmd ");DEBUG(cmd);DEBUG(" ");DEBUGLN(arg);
+  switch (cmd)
+  {
+#if defined(KEY_SOUND)
+    case KEY_SOUND_UP:
+      sound_up = arg;
+      break;
+    case KEY_SOUND_DOWN:
+      sound_down = arg;
+      break;
+#endif
+
+#if defined(KEY_VOLUME)
+    case KEY_VOLUME_UP:
+      volume_up = arg;
+      break;
+    case KEY_VOLUME_DOWN:
+      volume_down = arg;
+      break;
+#endif
+
+#if defined(KEY_TEMPERATURE)
+    case KEY_TEMPERATURE_UP:
+      temp_up = ((float)arg) / 10.0;
+      break;
+    case KEY_TEMPERATURE_DOWN:
+      temp_down = ((float)arg) / 10.0;
+      break;
+#endif
+
+#if defined(KEY_RELAY)
+    case KEY_RELAY:
+      relay_on = !!arg;
+      digitalWrite(RELAY_ON, relay_on ? HIGH : LOW);
+      digitalWrite(RELAY_OFF, relay_on ? LOW : HIGH);
+      delay(10);
+      digitalWrite(RELAY_ON, LOW);
+      digitalWrite(RELAY_OFF, LOW);
+      break;
+#endif
+
+    default:
+      break;
+  }
+}
 
 void pulseCounter(void)
 {
