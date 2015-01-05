8f9f9ca3e33fe4d12b858fbece14689b86dcc4ef
sushyad <sushyad@gmail.com>
2015-01-05 09:11:55 -0500

    Create GarageDoorController.ino
    Garage door controller running on Spark Core. Controls two garage doors using commands received on MQTT queue.

---
+++ GarageDoorController/GarageDoorController.ino
@@ -0,0 +1,60 @@
+// This #include statement was automatically added by the Spark IDE.
+#include "MQTT/MQTT.h"
+
+#define RELAY_PULSE_MS            250  //just enough that the opener will pick it up
+
+char RELAYPIN_LEFT[] = {D6, D7};
+char RELAYPIN_RIGHT[] = {D4, D5};
+uint8_t mqttServer[] = { 192, 168, 0, 9 };
+
+void callback(char* topic, byte* payload, unsigned int length);
+MQTT client(mqttServer, 1883, callback);
+
+// recieve message
+void callback(char* topicChar, byte* payload, unsigned int length) {
+    char p[length + 1];
+    memcpy(p, payload, length);
+    p[length] = NULL;
+    String message(p);
+    String topic(topicChar);
+    
+    if (topic.equals("wsn/garageDoorLeft/command")) {
+        pulseRelay(RELAYPIN_LEFT);
+    } else if (topic.equals("wsn/garageDoorRight/command")) {
+        pulseRelay(RELAYPIN_RIGHT);
+    }
+}
+
+
+void setup() {
+    pinMode(RELAYPIN_LEFT[0], OUTPUT);
+    pinMode(RELAYPIN_LEFT[1], OUTPUT);
+    pinMode(RELAYPIN_RIGHT[0], OUTPUT);
+    pinMode(RELAYPIN_RIGHT[1], OUTPUT);
+    digitalWrite(RELAYPIN_LEFT[0], HIGH);
+    digitalWrite(RELAYPIN_LEFT[1], HIGH);
+    digitalWrite(RELAYPIN_RIGHT[0], HIGH);
+    digitalWrite(RELAYPIN_RIGHT[1], HIGH);
+    
+    // connect to the server
+    client.connect("garageDoorController");
+
+    // publish/subscribe
+    if (client.isConnected()) {
+        client.subscribe("wsn/+/command");
+    }
+}
+
+void pulseRelay(char relayPin[])
+{
+    digitalWrite(relayPin[0], LOW);
+    digitalWrite(relayPin[1], LOW);
+    delay(RELAY_PULSE_MS);
+    digitalWrite(relayPin[0], HIGH);
+    digitalWrite(relayPin[1], HIGH);
+}
+
+void loop() {
+    if (client.isConnected())
+        client.loop();
+}
