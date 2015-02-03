7e2060630416c518aec78cf36b510906d7bcf8bf
Susheel Yadav <sushyad@gmail.com>
2015-02-03 11:23:02 -0500

    Added inverter and reduce bootup glitches
    Added inverter to prevent garage door from opening at powerup and reset.
    Added code to reduce occurences of garage door opening due to bootup glitches.

--- GarageDoorController/GarageDoorController.ino
+++ GarageDoorController/GarageDoorController.ino
@@ -1,11 +1,30 @@
 // This #include statement was automatically added by the Spark IDE.
-#include "MQTT/MQTT.h"
+#include "Mqtt.h"
 
 #define RELAY_PULSE_MS            250  //just enough that the opener will pick it up
 
-char RELAYPIN_LEFT[] = {D6, D7};
-char RELAYPIN_RIGHT[] = {D4, D5};
+char RELAYPIN_LEFT[] = {D7, D6};
+char RELAYPIN_RIGHT[] = {D5, D4};
+int numberOfPins = 1;
+
 uint8_t mqttServer[] = { 192, 168, 0, 9 };
+
+class RunBeforeSetup {
+public:
+	RunBeforeSetup() {
+	    for (int index = 0; index < numberOfPins; index++)  {
+    	    PIN_MAP[RELAYPIN_RIGHT[index]].gpio_peripheral->BSRR = PIN_MAP[RELAYPIN_RIGHT[index]].gpio_pin;
+	        pinMode(RELAYPIN_RIGHT[index], OUTPUT);
+	        digitalWrite(RELAYPIN_RIGHT[index], LOW);
+	        
+    	    PIN_MAP[RELAYPIN_LEFT[index]].gpio_peripheral->BSRR = PIN_MAP[RELAYPIN_LEFT[index]].gpio_pin;
+	        pinMode(RELAYPIN_LEFT[index], OUTPUT);
+	        digitalWrite(RELAYPIN_LEFT[index], LOW);
+        }
+	}
+};
+
+RunBeforeSetup runBeforeSetup;
 
 void callback(char* topic, byte* payload, unsigned int length);
 MQTT client(mqttServer, 1883, callback);
@@ -25,22 +44,21 @@ void callback(char* topicChar, byte* payload, unsigned int length) {
     }
 }
 
-
 void setup() {
-    pinMode(RELAYPIN_LEFT[0], OUTPUT);
-    pinMode(RELAYPIN_LEFT[1], OUTPUT);
-    pinMode(RELAYPIN_RIGHT[0], OUTPUT);
-    pinMode(RELAYPIN_RIGHT[1], OUTPUT);
-    digitalWrite(RELAYPIN_LEFT[0], HIGH);
-    digitalWrite(RELAYPIN_LEFT[1], HIGH);
-    digitalWrite(RELAYPIN_RIGHT[0], HIGH);
-    digitalWrite(RELAYPIN_RIGHT[1], HIGH);
-    
     connectToQueue();
+}
+
+void pulseRelay(char relayPin[]) {
     
-    //Register REST services
-    Spark.function("garageDoorLeft", toggleGarageDoorLeft);
-    Spark.function("garageDoorRight", toggleGarageDoorRight)    
+    for (int index = 0; index < numberOfPins; index++)  {
+        digitalWrite(relayPin[index], HIGH);
+    }
+    
+    delay(RELAY_PULSE_MS);
+    
+    for (int index = 0; index < numberOfPins; index++)  {
+        digitalWrite(relayPin[index], LOW);
+    }
 }
 
 void loop() {
@@ -51,45 +69,10 @@ void loop() {
     }
 }
 
-void pulseRelay(char relayPin[]) {
-    digitalWrite(relayPin[0], LOW);
-    digitalWrite(relayPin[1], LOW);
-    delay(RELAY_PULSE_MS);
-    digitalWrite(relayPin[0], HIGH);
-    digitalWrite(relayPin[1], HIGH);
-}
-
 void connectToQueue() {
     client.connect("garageDoorController");
     // publish/subscribe
     if (client.isConnected()) {
         client.subscribe("wsn/+/command");
-    }
-}
-
-int toggleGarageDoorLeft(String command)
-{
-    // write to the appropriate pin
-    pulseRelay(RELAYPIN_LEFT);
-    
-    //send a confirmation on MQTT queue
-    sendConfirmation("wsn/garageDoorLeft/confirmation");
-
-    return 1;
-}
-
-int toggleGarageDoorRight(String command)
-{
-    // write to the appropriate pin
-    pulseRelay(RELAYPIN_RIGHT);
-    //send a confirmation on MQTT queue
-    sendConfirmation("wsn/garageDoorRight/confirmation");
-    
-    return 1;
-}
-
-void sendConfirmation(String queue) {
-    if (client.isConnected()) {
-        client.publish(queue, "Toggled");
     }
 }
